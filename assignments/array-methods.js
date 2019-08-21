2852a3573055263f456c9f19ce98575ff934a0dd
vishalicious213 <vishalicious213@gmail.com>
2019-08-21 16:31:28 -0400

    array-methods 4 done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -119,7 +119,20 @@ console.log(runnersLargeSizeShirt);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
 let ticketPriceTotal = 0;
+
+// use "donation" variable. .reduce iterates over each item in array
+
+ticketPriceTotal = runners.reduce(function(count, runner) {
+  // console.log(runner.donation); test
+  ticketPriceTotal = ticketPriceTotal + runner.donation;
+  return ticketPriceTotal;
+  
+});
+
 console.log(ticketPriceTotal);
+
+//----------------------------------------------------------------------
+
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
